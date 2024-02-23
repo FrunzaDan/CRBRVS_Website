dca4917c816b1fe72b722fc26bf3dbd514367dcd
FrunzaDan <frunzadan96@gmail.com>
2024-02-24 00:01:11 +0200

    renamings

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -21,24 +21,24 @@ function sendEmail() {
     from_message: document.getElementById("from_message").value,
   };
 
-  document.getElementById("emailPopupTextHeader").innerHTML = "Hi " + from_name.value;
-  document.getElementById("emailPopupTextParagraph").innerHTML = "Sending... ";
+  document.getElementById("email-popup-text-header").innerHTML = "Hi " + from_name.value;
+  document.getElementById("email-popup-text-paragraph").innerHTML = "Sending... ";
 
   emailjs.send(serviceID, templateID, params, publicKey)
     .then(function (response) {
       if (response.status == 200) {
         console.log('EMAIL SUCCESS!', response.status, response.text);
 
-        document.getElementById("emailPopupTextParagraph").innerHTML = "Your message was sent successfully! ";
+        document.getElementById("email-popup-text-paragraph").innerHTML = "Your message was sent successfully! ";
       }
       else {
         console.log('EMAIL FAILED...', error);
-        document.getElementById("emailPopupTextParagraph").innerHTML = "There was a problem in processing your message. Please send an E-mail to crbrvsraps@gmail.com. ";
+        document.getElementById("email-popup-text-paragraph").innerHTML = "There was a problem in processing your message. Please send an E-mail to crbrvsraps@gmail.com. ";
       }
 
     }, function (error) {
       console.log('EMAIL FAILED...', error);
-      document.getElementById("emailPopupTextParagraph").innerHTML = "Our servers are too full. Please send an E-mail to crbrvsraps@gmail.com. ";
+      document.getElementById("email-popup-text-paragraph").innerHTML = "Our servers are too full. Please send an E-mail to crbrvsraps@gmail.com. ";
     });
 
   openPopup();
