bb16d263b1b26536c8e18285069b7fba1d69116c
FrunzaDan <frunzadan96@gmail.com>
2024-03-07 20:29:44 +0200

    loader

---
+++ js/windowLoaded.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", (event) => {
+    loadingOverlay = document.getElementById("loading-overlay");
+    loadingOverlay.style.opacity = 0;
+    setTimeout(() => {
+        loadingOverlay.style.display = "none";
+    }, 1000);
+});
