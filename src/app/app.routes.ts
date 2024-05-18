b3ae1613892b78787c5c37c0312488c1d7b4eda0
FrunzaDan <frunzadan96@gmail.com>
2024-05-18 22:56:55 +0300

    revision

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { MusicComponent } from './components/music/music.component';
+import { MerchComponent } from './components/merch/merch.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'music',
+    component: MusicComponent,
+    title: 'Music',
+  },
+  {
+    path: 'merch',
+    component: MerchComponent,
+    title: 'Merch',
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+    title: 'Contact',
+  },
+];
