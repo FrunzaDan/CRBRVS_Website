de54c93252712ea5bff8f885e5f96c0062dc45ce
FrunzaDan <frunzadan96@gmail.com>
2024-05-19 21:04:20 +0300

    revision

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { ViewportScroller } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -10,9 +11,13 @@ import { RouterModule } from '@angular/router';
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-  constructor(private viewportScroller: ViewportScroller) {}
+  constructor(
+    private viewportScroller: ViewportScroller,
+    private router: Router
+  ) {}
 
   public scrollToSection(elementId: string): void {
     this.viewportScroller.scrollToAnchor(elementId);
+    this.router.navigate([], { fragment: elementId });
   }
 }
