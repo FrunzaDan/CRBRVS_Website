35355059f436e65eb43ffe99cf3ec5b7b18c73e2
FrunzaDan <frunzadan96@gmail.com>
2025-02-09 01:12:43 +0200

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { Analytics } from '@angular/fire/analytics';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
   title: string = 'CRBRVS';
-
-  constructor(private fireAnalytics: Analytics) {}
 
   ngOnInit(): void {}
 }
