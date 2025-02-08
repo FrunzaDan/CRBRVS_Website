ab32633f4e797ea9c94c17159663beed21fceecd
FrunzaDan <frunzadan96@gmail.com>
2025-02-08 22:58:44 +0200

    refactor

--- src/app/services/load-music.service.ts
+++ src/app/services/load-music.service.ts
@@ -1,10 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/internal/Observable';
-import { Song } from '../interfaces/song';
-import { catchError } from 'rxjs/internal/operators/catchError';
-import { of } from 'rxjs/internal/observable/of';
 import musicData from '../../assets/music-list.json'; // Importing the JSON data directly
+import { Song } from '../interfaces/song';
 
 @Injectable({
   providedIn: 'root',
